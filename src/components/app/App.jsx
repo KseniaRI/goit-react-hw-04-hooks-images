c873be4d716c8d6c2b41d8a94be1c2974922561e
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-07-15 15:43:48 +0200

    slow scroll

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect } from 'react';
+import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { PixabayApiService } from '../../services/pixabay-api';
 import { Searchbar } from '../searchbar/Searchbar';
 import { ImageGallery } from '../image-gallery/ImageGallery';
@@ -8,11 +8,12 @@ import { Loader } from '../loader/Loader';
 import { Button } from '../button/Button';
 import { Modal } from '../modal/Modal';
 
-
 const PER_PAGE = 12;
 const pixabayApiService = new PixabayApiService(); 
 
 export const App = () => {
+
+  const galleryRef = useRef();
 
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
@@ -21,25 +22,23 @@ export const App = () => {
   const [totalImages, setTotalImages] = useState(0);
   const [status, setStatus] = useState('idle');
   const [selectedImgUrl, setSelectedImgUrl] = useState('');
-  // const [firstElOfPageId, setFirstElOfPageId] = useState('');
 
   useEffect(() => {
     
-  async function getImages() {
-    try {
-        const {hits, totalHits} = await pixabayApiService.fetchImages();
+    async function getImages() {
+      try {
+        const { hits, totalHits } = await pixabayApiService.fetchImages();
         
         if (hits.length > 0) {
           setImages(prevState => [...prevState, ...hits]);
           setStatus('resolved');
           setTotalImages(totalHits);
-
-      } else {
+        } else {
           setError(`There are no images with key word ${query}`);
           setStatus('rejected');
         }
       }
-      catch (error){
+      catch (error) {
         setError(error.message);
         setStatus('rejected');
       }
@@ -52,56 +51,56 @@ export const App = () => {
       pixabayApiService.page = page;
       
       getImages();
-    }        
+    }
   }, [query, page]);
 
-  
-  // const scrollToNextPage = () => {
-  //   const headerHeight = 90;
-  //   if (page > 1) {
-  //     const firstElOnPagePos = document.getElementById(firstElOfPageId).offsetTop - headerHeight;
-  //     window.scrollTo({
-  //       top: firstElOnPagePos,
-  //       behavior: 'smooth',
-  // });
-  //  }
-  // };
-  
   useLayoutEffect(() => {
-    const CARD_HEIGHT = 260;
+    
+    function slowScroll() {
+      if (page > 1) {
+        console.log(galleryRef.current.firstChild.getBoundingClientRect());
+        const { height: cardHeight } = galleryRef.current.firstChild.getBoundingClientRect();
 
-      window.scrollTo({
-      //  top: document.documentElement.scrollHeight,
-        top: CARD_HEIGHT * 2 * page,
-        behavior: 'smooth',
-      });
+        window.scrollBy({
+          top: cardHeight * 3 * page,
+          behavior: 'smooth',
+        })
+      }
+    }
+    
+    document.addEventListener("scroll", slowScroll);
+
+    //     window.scrollTo({
+    //      top: document.documentElement.scrollHeight,
+    //       behavior: 'smooth',
+    //     });
   });
 
-  const formSubmitHandler = ({ query }, { resetForm }) => {
-    setPage(1);
-    setQuery(query);
-    setImages([]);
+    const formSubmitHandler = ({ query }, { resetForm }) => {
+      setPage(1);
+      setQuery(query);
+      setImages([]);
 
-    resetForm();
-  }
+      resetForm();
+    }
     
-  return (
+    return (
       <Container>
         <Searchbar onSubmit={formSubmitHandler} />
         {status === 'idle' && <Message text="Enter something..." />}
         {status === 'rejected' && <Message text={error} />}
         {status === 'pending' && <Loader />}
         {status === 'resolved' && <>
-          <ImageGallery images={images} onShowModal={setSelectedImgUrl} />
+          <ImageGallery ref={galleryRef} images={images} onShowModal={setSelectedImgUrl} />
           
         </>}
-      {status === 'resolved' && Math.ceil(totalImages / PER_PAGE) !== page &&
-        <Button onClick={() => setPage(prevState => prevState + 1)} /> }
+        {status === 'resolved' && Math.ceil(totalImages / PER_PAGE) !== page &&
+          <Button onClick={() => setPage(prevState => prevState + 1)} />}
         {selectedImgUrl.length > 0 && (
-                 <Modal onClose={() => setSelectedImgUrl('')}>
-                    <img src={selectedImgUrl} alt="" />
-                 </Modal>
-            )}
+          <Modal onClose={() => setSelectedImgUrl('')}>
+            <img src={selectedImgUrl} alt="" />
+          </Modal>
+        )}
       </Container>
     )
 }
--- src/components/image-gallery/ImageGallery.jsx
+++ src/components/image-gallery/ImageGallery.jsx
@@ -1,17 +1,18 @@
+import { forwardRef} from "react";
 import { ImageGalleryItem } from '../image-gallery-item/ImageGalleryItem';
 import { Grid } from './ImageGallery.styled';
 import PropTypes from 'prop-types'; 
 
-export const ImageGallery = ({ images, onShowModal}) => {
+export const ImageGallery = forwardRef(({ images, onShowModal}, ref) => {
 
     return (
         <>
-            <Grid>
+            <Grid ref={ref}>
                 {images.map(image => <ImageGalleryItem onShowModal={onShowModal} image={image} key={image.id}/>)}     
             </Grid>
         </>
     );       
-}
+})
 
 ImageGallery.propTypes = {
     images: PropTypes.arrayOf(PropTypes.shape({
